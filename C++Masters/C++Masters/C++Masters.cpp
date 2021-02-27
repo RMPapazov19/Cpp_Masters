08fa50b13d1feb55bb615b14cd84a96b5c36545e
Danislav Todorov <DATodorov19@codingburgas.bg>
2021-02-27 23:38:17 +0200

    Add front-end

---
+++ C++Masters/C++Masters/C++Masters.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	cout << "|*";
+	for (int i = 0; i < 15; i++)
+	{
+		cout << "-";
+	}
+	cout << "C++Maters";
+	for (int i = 0; i < 15; i++)
+	{
+		cout << "-";
+	}
+	cout << "*|" << endl << endl;
+	cout << "   CHOOSE TOPIC:" << endl;
+	cout << "       1) History" << endl;
+	cout << "       2) Food" << endl;
+	cout << "       3) Animals" << endl;
+	cout << "       4) Gaming" << endl;
+	cout << "       5) Computers" << endl << endl;
+	cout << "|*";
+	for (int i = 0; i < 15; i++)
+	{
+		cout << "-";
+	}
+	cout << "C++Maters";
+	for (int i = 0; i < 15; i++)
+	{
+		cout << "-";
+	}
+	cout << "*|" << endl << endl;
+	cout << "CHOICE: ";
+	int topic;
+	cin >> topic;
+}
